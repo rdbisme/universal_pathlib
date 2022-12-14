b862e70cd8d84a220ddb7c2fc4f869e47021af93
Ruben DI BATTISTA <ruben.di_battista@qube-rt.com>
2022-12-14 15:50:03 +0100

    Don't use useless posixify

--- upath/tests/conftest.py
+++ upath/tests/conftest.py
@@ -14,7 +14,6 @@ from fsspec.registry import register_implementation, _registry
 
 import fsspec
 
-from .utils import posixify
 
 
 def pytest_addoption(parser):
@@ -194,7 +193,7 @@ def s3_fixture(s3_server, local_testdir):
     else:
         s3.mkdir(bucket_name)
     for x in Path(local_testdir).glob("**/*"):
-        target_path = f"{bucket_name}/{posixify(x.relative_to(local_testdir))}"
+        target_path = f"{bucket_name}/{x.relative_to(local_testdir).as_posix()}"
         if x.is_file():
             s3.upload(str(x), target_path)
     s3.invalidate_cache()
@@ -254,7 +253,7 @@ def gcs_fixture(docker_gcs, local_testdir):
     else:
         gcs.mkdir(bucket_name)
     for x in Path(local_testdir).glob("**/*"):
-        target_path = f"{bucket_name}/{posixify(x.relative_to(local_testdir))}"
+        target_path = f"{bucket_name}/{x.relative_to(local_testdir).as_posix()}"
         if x.is_file():
             gcs.upload(str(x), target_path)
     gcs.invalidate_cache()
--- upath/tests/utils.py
+++ upath/tests/utils.py
@@ -12,7 +12,3 @@ def only_on_windows(func):
     return pytest.mark.skipif(
         not sys.platform.startswith("win"), reason="Only run on Windows"
     )(func)
-
-
-def posixify(path):
-    return str(path).replace("\\", "/")
