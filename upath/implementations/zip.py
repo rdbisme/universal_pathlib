9b7efb297418459c0e4be73547eb9da4eb8011b4
Ruben DI BATTISTA <ruben.di_battista@qube-rt.com>
2022-12-15 13:35:50 +0100

    :recycle: refactor: Simplify logic

--- upath/implementations/zip.py
+++ upath/implementations/zip.py
@@ -1,7 +1,7 @@
 from __future__ import annotations
 
 import re
-from typing import TYPE_CHECKING, TypeVar
+from typing import TYPE_CHECKING, Generator, TypeVar
 from urllib.parse import ParseResult
 
 from fsspec.implementations.zip import ZipFileSystem
@@ -18,83 +18,32 @@ class _ZipAccessor(upath.core._FSSpecAccessor):
         self._fs = ZipFileSystem(f"{parsed_url.netloc}")
 
     def _format_path(self, path: ZipPath):
-        ret = re.sub(rf"^{path._url.netloc}", "", path.path)
-
-        if not (ret):
-            return "/"
-
-        return ret.lstrip("/")
+        return super()._format_path(path).lstrip("/")
 
 
 PT = TypeVar("PT", bound="ZipPath")
 class ZipPath(upath.core.UPath):
     _default_accessor = _ZipAccessor
 
-    @classmethod
-    def _from_parts(cls, args, url=None, **kwargs):
-        url_parts = url.path.split("/")
-        zip_ext_index = (
-            next(
-                (
-                    url_parts.index(part)
-                    for part in url_parts
-                    if part.split(".")[-1] == "zip"
-                )
-            )
-            + 1
-        )
+    def iterdir(self: PT) -> Generator[PT, None, None]:
+        """Iterate over the files in this directory.  Does not yield any
+        result for the special paths '.' and '..'.
+        """
+        if self.is_file():
+            raise NotADirectoryError
 
-        netloc = url.netloc + "/".join(url_parts[:zip_ext_index])
-
-        path = "/".join(url_parts[zip_ext_index:])
-
-        if path:
-            path = "/" + path
-
-        url = url._replace(path=path, netloc=netloc)
-
-        return super()._from_parts(args, url, **kwargs)
-
-    @classmethod
-    def _format_parsed_parts(
-        cls: type[PT],
-        drv: str,
-        root: str,
-        parts: list[str],
-        url: SplitResult | None = None,
-        **kwargs: Any,
-    ) -> str:
-        if parts:
-            join_parts = parts[1:] if parts[0] == "/" else parts
-        else:
-            join_parts = []
-        if drv or root:
-            path: str = drv + root + cls._flavour.join(join_parts)
-        else:
-            path = cls._flavour.join(join_parts)
-        if not url:
-            scheme: str = kwargs.get("scheme", "file")
-            netloc: str = kwargs.get("netloc", "")
-        else:
-            scheme, netloc = url.scheme, url.netloc
-
-        path = path.replace(netloc, "")
-        scheme = scheme + ":"
-        netloc = "//" + netloc if netloc else ""
-
-        formatted = scheme + netloc + path
-        return formatted
-
-    def iterdir(self):
         for name in self._accessor.listdir(self):
             # fsspec returns dictionaries
             if isinstance(name, dict):
-                name = name.get("name")
+                name = name.get("name").rstrip("/")
+
+            if name == self.name: 
+                continue
+
             if name in {".", ".."}:
                 # Yielding a path object for these makes little sense
                 continue
             # only want the path name with iterdir
-            name = name.rstrip("/").split("/")[-1]
             name = self._sub_path(name)
             yield self._make_child_relpath(name)
 
--- upath/tests/implementations/test_zip.py
+++ upath/tests/implementations/test_zip.py
@@ -27,32 +27,6 @@ class TestUPathZip(BaseTests):
 
         assert not (self.path / "not-existing-file.txt").is_file()
 
-    def test_iterdir(self, local_testdir):
-        pl_path = Path(local_testdir)
-
-        up_iter = list(self.path.iterdir())
-        pl_iter = list(pl_path.iterdir())
-
-        for x in up_iter:
-            assert x.name != ""
-            assert x.exists()
-
-        assert len(up_iter) == len(pl_iter)
-        assert set(p.name for p in pl_iter) == set(u.name for u in up_iter)
-        # assert next(self.path.parent.iterdir()).exists()
-
-    def test_iterdir2(self, local_testdir):
-        pl_path = Path(local_testdir) / "folder1"
-
-        up_iter = list((self.path / "folder1").iterdir())
-        pl_iter = list(pl_path.iterdir())
-
-        for x in up_iter:
-            assert x.exists()
-
-        assert len(up_iter) == len(pl_iter)
-        assert set(p.name for p in pl_iter) == set(u.name for u in up_iter)
-        # assert next(self.path.parent.iterdir()).exists()
 
     @pytest.mark.xfail(
         reason="Current fsspec ZipFileSystem implementation is read only"
